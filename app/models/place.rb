5b33d6b8e571f3045bfbd012ce71eeaddd1d48be
Minh Hoang Nguyen <mnguyen83@academic.rrc.ca>
2022-10-02 15:14:59 -0500

    Add Model Associations: One user creates many places. A place is created by a user. A place has a type. A type has many places. A user like many places. A place is liked by many users.

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,2 +1,7 @@
 class Place < ApplicationRecord
+  belongs_to :user
+  belongs_to :type
+
+  has_many :user_places
+  has_many :users, through: :user_places
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  has_many :places
+
+  has_many :user_places
+  has_many :places, through: :user_places
 end
