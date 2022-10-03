5c15e49b28e31073e9e0d64294ea773d0f0ec6e6
Minh Hoang Nguyen <mnguyen83@academic.rrc.ca>
2022-10-03 12:48:05 -0500

    Implement the About page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+
+  root to: "places#index"
+
+  get 'places/index'
+  get 'places/show'
+  get 'about', to: "about#index"
 end
