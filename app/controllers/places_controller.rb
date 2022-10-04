99a30b54fea5703fac150a13292b79dfe7c9ac8c
Minh Hoang Nguyen <mnguyen83@academic.rrc.ca>
2022-10-04 15:11:04 -0500

    Fix errors generated by HTML validations

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -11,7 +11,7 @@ class PlacesController < ApplicationController
     zoom = 9
     bearing = 0
     geojson = "geojson(%7B%22type%22%3A%22Point%22%2C%22coordinates%22%3A%5B#{longitude}%2C#{latitude}%5D%7D)"
-    @map_link = "https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/#{geojson}/#{longitude},#{latitude},#{zoom},#{bearing}/300x200?access_token=pk.eyJ1IjoibW5ndXllbjgzIiwiYSI6ImNsOGJ0ZDVtMTA5b3k0MXA2eDYwaW93OW4ifQ.RLhT0HdE3NeEsTshFeh9Hg"
+    @map_link = "https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/#{geojson}/#{longitude},#{latitude},#{zoom},#{bearing}/500x400?access_token=pk.eyJ1IjoibW5ndXllbjgzIiwiYSI6ImNsOGJ0ZDVtMTA5b3k0MXA2eDYwaW93OW4ifQ.RLhT0HdE3NeEsTshFeh9Hg"
   end
 
   def search
