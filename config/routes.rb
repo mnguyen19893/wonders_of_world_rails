6c20964d478817f5889ebcf758caace4ae6c7dd0
Minh Hoang Nguyen <mnguyen83@academic.rrc.ca>
2022-10-04 10:10:34 -0500

    Implement simple search

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,12 @@ Rails.application.routes.draw do
   root to: "places#index"
   get 'about', to: "about#index"
 
+  resources :places, only: [:index, :show] do
+    collection do
+      get 'search'
+    end
+  end
 
-  resources :places, only: [:index, :show]
   resources :users, only: [:index, :show]
   resources :types, only: [:index, :show]
 
